888ae6aa9dece21f883ffbf580807782be21f71c
Luke Ledvina <lukeledvina19@gmail.com>
2024-06-11 05:28:04 -0400

    Added ball functionality

--- Ball/ball.gd
+++ Ball/ball.gd
@@ -7,6 +7,7 @@ extends Area2D
 var direction: Vector2 = Vector2.ZERO
 var initial_speed: int = 100
 var speed: int = initial_speed
+var velocity: Vector2
 
 func _ready():
 	await get_tree().create_timer(0.5).timeout
@@ -15,10 +16,19 @@ func _ready():
 
 func _physics_process(delta):
 	
-	var velocity = direction * speed
+	velocity = direction * speed
 	
 	position += velocity * delta
 
+func _on_area_left_body_entered(body):
+	direction.x = -direction.x
 
-func _on_body_entered(body):
-	print(body)
+func _on_area_up_body_entered(body):
+	direction.y = -direction.y
+
+func _on_area_down_body_entered(body):
+	direction.y = -direction.y
+
+func _on_area_right_body_entered(body):
+	direction.x = -direction.x
+
