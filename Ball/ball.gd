3abd12f29fcc72af466595bae15c5acf07cb90fe
Luke Ledvina <lukeledvina19@gmail.com>
2024-06-11 05:54:05 -0400

    Started working on player interaction with ball

--- Ball/ball.gd
+++ Ball/ball.gd
@@ -21,13 +21,37 @@ func _physics_process(delta):
 	position += velocity * delta
 
 func _on_area_left_body_entered(body):
+	if body.name == "block":
+		#increment score depending on the type of block ( color )
+		#increase the speed of the ball
+		#destroy the block
+		pass
+	
 	direction.x = -direction.x
 
 func _on_area_up_body_entered(body):
 	direction.y = -direction.y
 
 func _on_area_down_body_entered(body):
+	
+	
+	if body.is_in_group("block"):
+		#increment score depending on the type of block ( color )
+		#increase the speed of the ball
+		#destroy the block
+		pass
+	elif body.name == "Player":
+		#reflect differently depending on what area of player hit
+		# get the global position of the ball and the player, depending of their difference, reflect accordingly with a gradient
+		
+		#THIS IS BACKWARDS, THE MORE TO THE EDGES (HIGHER DIFFERENCE), THE LESS Y DIRECTION SHOULD BE AND THE GREATER X DIRECTION SHOULD BE... GET TO THINKING...!
+		var player_position_x: float = body.position.x
+		var difference = player_position_x - position.x
+		direction = Vector2(direction.x, direction.y * difference).normalized()
+		
 	direction.y = -direction.y
+	
+	
 
 func _on_area_right_body_entered(body):
 	direction.x = -direction.x
